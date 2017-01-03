060da4b34e91f9bdfa6403616441023f92573f2a
Ryan Henness <ryanhenness@gmail.com>
2017-01-02 17:05:29 -0800

    Created correct exports - Routes can now access Pg functions to retrieve data

--- routes/pg.js
+++ routes/pg.js
@@ -1,23 +1,25 @@
 module.exports = function(app){
 	// Connect to the pg database
 	// req.body.username
-	var connect = process.env.PG_CONNECT;
-	var pg = require('pg'),
-  		bodyParser = require('body-parser'),
-  		path = require('path');
-	pg.defaults.ssl = true;
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: false}));
+  require('./passport.js')(app);
+  var connect = process.env.PG_CONNECT;
+  var pg = require('pg'),
+      bodyParser = require('body-parser'),
+      path = require('path');
+  pg.defaults.ssl = true;
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false}));
 
-  app.post('/lightingpreference', loggedIn, function(req, res){
+  app.post('/set-preferences', loggedIn, function(req, res){
     pg.connect(connect, function(err, client, done){
       if(err){
         return console.error('error fetching', err);
       }
-      client.query('INSERT INTO public.users(fbid, lighting) VALUES('+req.user.id+','+req.body.lighting+')');
+      client.query('INSERT INTO public.users(fbid, set) VALUES('+req.user.id+',TRUE)');
       done();
       res.redirect('/');
     });
+    pg.end();
   });
 
   function loggedIn(req, res, next) {
@@ -27,4 +29,14 @@ module.exports = function(app){
         res.redirect('/login');
     }
   }
+
+  var GetUsers = function GetUsers(id){
+      pg.connect(connect, function(err, client, done){
+      client.query('SELECT * FROM users WHERE fbid=$1', [id], function(err, result){
+        console.log(result.rows);
+      });
+    }); 
+  };
+  module.exports.GetUsers = GetUsers;
 };
+
--- routes/routes.js
+++ routes/routes.js
@@ -1,32 +1,34 @@
 module.exports = function(app){
 	// routes must include passport
 	require('./passport.js')(app);
+	var pg = require('./pg');
 
 	app.get('/', loggedIn, function(req, res){
-    	res.render('index', {user: req.user});
-  	});
+    pg.GetUsers(req.user.id);
+    res.render('index', {user: req.user});
+  });
 
 	app.get('/preferences', loggedIn, function(req, res){
-    	res.render('preferences', {user: req.user});
-  	});
+    res.render('preferences', {user: req.user});
+	});
 
 	app.get('/scenes', loggedIn, function(req, res){
-    	res.render('scenes', {user: req.user});
-  	});
+    res.render('scenes', {user: req.user});
+ 	});
 
-  	app.get('/login', function(req, res){
-    	res.render('login', {user: req.user});
-  	});
+  app.get('/login', function(req, res){
+    res.render('login', {user: req.user});
+  });
 
-  	app.get('/scene', function(req, res){
-    	res.render('scene', {query: req.query});
-  	});
+  app.get('/scene', loggedIn, function(req, res){
+    res.render('scene', {query: req.query});
+  });
 
-  	function loggedIn(req, res, next) {
-    	if (req.user) {
-        	next();
-    	} else {
-        	res.redirect('/login');
-    	}
+  function loggedIn(req, res, next) {
+   	if (req.user) {
+       	next();
+   	} else {
+       	res.redirect('/login');
+   	}
 	}
 };
